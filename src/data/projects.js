6ad3a2bbf17819e0b75c754041595e424cce388e
Chris Gilbert <chris.gilbert@multiverse.io>
2021-12-22 13:50:51 +0000

    WIP: add basic projects page

---
+++ src/data/projects.js
@@ -0,0 +1,9 @@
+export default [
+	{
+		name: 'Bias',
+		url: 'https://biaseditions.com/',
+		src: 'bias.png',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+	}
+];
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,8 +1,48 @@
-<div>🚧</div>
+<script>
+	import projects from '../data/projects.js';
+</script>
+
+<header>
+	<h1>Projects</h1>
+</header>
+<ul>
+	{#each projects as project}
+		<li class="project">
+			<div class="project__child-container">
+				<h2 target="_blank" href={project.url}>
+					{project.name}
+				</h2>
+				<p>{project.description}</p>
+			</div>
+			<div class="project__child-container">
+				<img src={project.src} alt={project.name} />
+			</div>
+		</li>
+	{/each}
+</ul>
 
 <style>
-	div {
-		font-size: 56px;
+	header {
+		margin: var(--xl-spacer) 0;
+	}
+	header h1 {
 		text-align: center;
+	}
+	.project {
+		display: flex;
+		align-items: center;
+		grid-gap: var(--l-spacer);
+	}
+
+	.project__child-container {
+		flex: 1;
+	}
+
+	.project__child-container a {
+		color: var(--white);
+	}
+	.project__child-container img {
+		height: 100%;
+		width: 100%;
 	}
 </style>
